ae17936e3b139c60c64046331aea6a64e254ec8f
yakud <yakudgm@gmail.com>
2020-03-13 20:22:03 +0300

    Swagger /v1/timeseries/sent-and-fees

--- swagger/generate.sh
+++ swagger/generate.sh
@@ -4,4 +4,4 @@
 #oapi-codegen -package authapi -generate types swagger-v1.yml > tonapi/swagger.types.gen.go
 #oapi-codegen -package authapi -generate server swagger-v1.yml > tonapi/swagger.server.gen.go
 #oapi-codegen -package authapi -generate client swagger-v1.yml > tonapi/swagger.client.gen.go
-oapi-codegen -package tonapi -generate spec swagger-v1.yml > tonapi/swagger.spec.gen.go
+oapi-codegen -package tonapi -generate spec swagger.yml > tonapi/swagger.spec.gen.go

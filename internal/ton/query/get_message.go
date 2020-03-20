5e672f33af0aa1878c97b4b37e9b5a288a30e011
yakud <yakudgm@gmail.com>
2020-03-20 06:54:52 +0300

    Account messages count

--- internal/ton/query/get_message.go
+++ internal/ton/query/get_message.go
@@ -15,7 +15,7 @@ const (
 		Messages.Init as MessagesInit,
 		Messages.Bounce as MessagesBounce,
 		Messages.Bounced as MessagesBounced,
-		if(Messages.CreatedAt != 0, Messages.CreatedAt, TrxTime) as MessagesCreatedAt,
+		if(Messages.CreatedAt != 0, Messages.CreatedAt, toUInt64(TrxTime)) as MessagesCreatedAt,
 		Messages.CreatedLt as MessagesCreatedLt,
 		Messages.ValueNanograms as MessagesValueNanograms,
 		Messages.ValueNanogramsLen as MessagesValueNanogramsLen,
